91cfb84fc2f83520a8130ed56d3756909e6cd2c0
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-15 19:30:16 -0700

    Add average_pages unit test to Author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Author, type: :model do
     it {should have_many :author_books}
     it {should have_many(:books).through(:author_books)}
   end
+
+  describe "methods" do
+    before(:each) do
+      @book_1 = Book.create(title: "Pride & Prejudice", publication_year: "1813", pages: 432)
+      @book_2 = Book.create(title: "Emma", publication_year: "1815", pages: 544)
+      @book_3 = Book.create(title: "Sense & Sensibility", publication_year: "1811", pages: 352)
+
+      @author_1 = Author.create(name: "Jane Austen")
+
+      @author_1.books << [@book_1, @book_2, @book_3]
+    end
+
+    it "#average_pages" do
+      expect(@author_1.average_pages.to_i).to eq(442)
+    end
+  end
 end
