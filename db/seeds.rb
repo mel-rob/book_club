ed05784173c1883f8dd3ed0cf4672d0fbc3fef98
Melissa Robbins <melissamrobbins@gmail.com>
2019-12-15 18:37:51 -0700

    Add seeds to file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@book_1 = Book.create(title: "Pride & Prejudice", publication_year: "1813", pages: 432)
+@book_2 = Book.create(title: "Emma", publication_year: "1815", pages: 544)
+@book_3 = Book.create(title: "Sense & Sensibility", publication_year: "1811", pages: 352)
+@book_4 = Book.create(title: "The Alchemist", publication_year: "1988", pages: 263)
+@book_5 = Book.create(title: "Tiny Pretty Things", publication_year: "2015", pages: 464)
+
+@author_1 = Author.create(name: "Jane Austen")
+@author_2 = Author.create(name: "Paulo Coelho")
+@author_3 = Author.create(name: "Sona Charaipotra")
+@author_4 = Author.create(name: "Dhonielle Clayton")
+
+@author_1.books << [@book_1, @book_2, @book_3]
+@author_2.books << [@book_4]
+@author_3.books << [@book_5]
+@author_4.books << [@book_5]
